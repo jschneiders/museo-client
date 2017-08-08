58b2d5eaea12a28c150a17a389fe7de1e3f99741
Janine <schneiders.janine@gmail.com>
2017-08-08 17:11:17 -0300

    Obras

---
+++ content/obras.php
@@ -0,0 +1,45 @@
+<?php
+
+$content = '<div class="container obras">
+                <div class="col-md-8 col-md-offset-2 text-center" >
+
+                    <h1>Museo (nome do atual museu)</h1>
+                    <h4>Navegue pelas obras do museu (nome do atual museu) </h4>
+
+
+                    <form class="obras pesquisar">
+                        <div class="form-group">
+                            <input type="text" class="form-control " placeholder="autor ou nome da obra">
+                            <button type="submit" class="btn btn-default ">pesquisar</button>
+                        </div>                        
+                    </form>
+
+                    <div class="panel panel-default">
+                        <!-- Default panel contents -->
+                        <div class="panel-heading">Resultado da busca</div>
+
+                        <!-- Table -->
+                        <table class="table">
+                            <tr>
+                                <td><a href="#" >Nome da obra</a></td>
+                                <td>Autores</td>
+                                <td>ano</td>
+                            </tr>
+                            <tr>
+                                <td><a href="#" >Nome da obra</a></td>
+                                <td>autores</td>
+                                <td>ano</td>
+                            </tr>
+                            <tr>
+                                <td><a href="#" >Nome da obra</a></td>
+                                <td>autores</td>
+                                <td>ano</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>';
+
+return $content;
+
+?>
