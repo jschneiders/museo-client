0d657b0156e817178aacb71dd6ab8430fde6a8c1
Victor Covalski <vrcjunes@inf.ufpel.edu.br>
2017-08-09 21:43:02 -0300

    fix no elasticmuseu

--- sql/connect.php
+++ sql/connect.php
@@ -1,9 +1,9 @@
 <?php
 
 //Db config
-$host= "localhost"; // Host name
+$host= "127.0.0.1:3306"; // Host name
 $username="root"; // Mysql username
-$password=""; // Mysql password
+$password="mypassword1"; // Mysql password
 $db_name="museo"; // Database name
 $connection = mysqli_connect("$host", "$username", "$password") or die(mysql_error());
 mysqli_select_db($connection, "$db_name") or die(mysql_error());
