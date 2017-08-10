afd10fb1ce9d995f0d80522afd5131874de0b8a4
Janine <schneiders.janine@gmail.com>
2017-08-09 21:21:54 -0300

    Tela obra layout

---
+++ content/obra.php
@@ -0,0 +1,27 @@
+<?php
+
+$content = '<div class="container obra">
+                <div class="col-md-8 col-md-offset-2 " >
+                    <a class="btn btn-default button-space" href="#" role="button">voltar</a>
+                    <div class="">
+                        <img src="https://oglobo.globo.com/fotos/2011/04/28/28_MHG_picasso.jpg" class="img-responsive" alt="nome da obra"/>
+                    </div>
+                    <div class="">
+                        <h3>Nome da obra</h3>
+                        <h5>Autor ou autores</h5>
+                        <p>Paragrafo de descrição Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut est mi, eleifend imperdiet tincidunt sed, scelerisque ut risus. Vivamus aliquet dolor luctus commodo faucibus. Mauris vel justo tincidunt, finibus ex at, tempus lorem. Aenean nec suscipit sapien, vitae dictum turpis. Donec vel fringilla dui, ut commodo justo. Nullam nec tincidunt mauris. Aenean viverra id leo venenatis fringilla. Duis eu ante consequat erat vestibulum malesuada id eget velit. Praesent quis nisi in est dictum maximus non at augue. Curabitur vestibulum sem sit amet dui pharetra iaculis.</p>
+                        <!-- informações para quem estiver logado-->
+                        <h5>ano</h5>
+                        <h5>outra informação</h5>
+                        <h5>outra informação</h5>
+                        <h5>outra informação</h5>
+                        <h5>outra informação</h5>
+                        <h5>outra informação</h5>
+                    </div>
+                    <a class="btn btn-default button-space" href="#" role="button">voltar</a>
+                </div>
+            </div>';
+
+return $content;
+
+?>
